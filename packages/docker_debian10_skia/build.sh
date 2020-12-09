c16dcfef75e3a066b3caac291dfc99bb9fc67c93
kimown <kimown@163.com>
2020-12-09 11:01:03 +0800

    chore:

--- packages/docker_debian10_skia/build.sh
+++ packages/docker_debian10_skia/build.sh
@@ -38,22 +38,22 @@ git checkout -b nodev1 0922403dcdc6b9ea70d39149a3a67295034f14ca
 git checkout nodev1
 
 cp /docker_debian10_skia/enable_gpu_render_skottie_v1/skottie2movie.cpp tools/skottie2movie.cpp
-#
-#python2 tools/git-sync-deps
-#unsetProxy
-#
-#sudo apt-get update && sudo apt -y install build-essential freeglut3-dev libfontconfig-dev libfreetype6-dev libgif-dev libgl1-mesa-dev libglu1-mesa-dev libharfbuzz-dev libicu-dev libjpeg-dev libpng-dev libwebp-dev 
-#sudo apt -y install clang
-#clang -v
-#bin/gn gen out/Static --args='skia_use_egl=true skia_use_ffmpeg=true cc="clang" cxx="clang++" extra_cflags_cc=["-fno-exceptions","-fexceptions"]'
-#python --version
-#sudo apt -y install libavutil-dev libavformat-dev libswscale-dev libavcodec-dev
-#pkg-config --libs libavcodec libavformat libswscale libavutil
-#
-#./ninja -C out/Static skottie2movie 
-#./ninja -C out/Static skottie_tool
-#
-#
-#./out/Static/skottie2movie || echo "skottie2movie"
-#./out/Static/skottie_tool || echo "skottie_tool"
+
+python2 tools/git-sync-deps
+unsetProxy
+
+sudo apt-get update && sudo apt -y install build-essential freeglut3-dev libfontconfig-dev libfreetype6-dev libgif-dev libgl1-mesa-dev libglu1-mesa-dev libharfbuzz-dev libicu-dev libjpeg-dev libpng-dev libwebp-dev 
+sudo apt -y install clang
+clang -v
+bin/gn gen out/Static --args='skia_use_egl=true skia_use_ffmpeg=true cc="clang" cxx="clang++" extra_cflags_cc=["-fno-exceptions","-fexceptions"]'
+python --version
+sudo apt -y install libavutil-dev libavformat-dev libswscale-dev libavcodec-dev
+pkg-config --libs libavcodec libavformat libswscale libavutil
+
+./ninja -C out/Static skottie2movie 
+./ninja -C out/Static skottie_tool
+
+
+./out/Static/skottie2movie || echo "skottie2movie"
+./out/Static/skottie_tool || echo "skottie_tool"
 
