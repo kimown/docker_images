8e8cd83d3606b6de086ef5eb5b97c7beb9aff58a
kimown <kimown@163.com>
2020-12-09 11:06:37 +0800

    chore:

--- packages/docker_debian10_skia/build.sh
+++ packages/docker_debian10_skia/build.sh
@@ -39,6 +39,8 @@ git checkout nodev1
 
 cp /docker_debian10_skia/enable_gpu_render_skottie_v1/skottie2movie.cpp tools/skottie2movie.cpp
 
+#https://stackoverflow.com/questions/38378914/how-to-fix-git-error-rpc-failed-curl-56-gnutls
+git config --global http.postBuffer 1048576000
 python2 tools/git-sync-deps
 unsetProxy
 
